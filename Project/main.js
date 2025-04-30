aa058de108170f3921d8d9205ae857d1afc7d81a
KJS02 <rkdwnstj0528@gmail.com>
2025-04-30 12:14:15 +0900

    3.0

--- Project/main.js
+++ Project/main.js
@@ -1,20 +1,27 @@
-var apiURI = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+"d52f29cf58fb03daa37388c645b836c6";
-        $.ajax({
-            url: apiURI,
-            dataType: "json",
-            type: "GET",
-            async: "false",
-            success: function(resp) {
-                console.log(resp);
-                console.log("현재온도 : "+ (resp.main.temp- 273.15) );
-                console.log("현재습도 : "+ resp.main.humidity);
-                console.log("날씨 : "+ resp.weather[0].main );
-                console.log("상세날씨설명 : "+ resp.weather[0].description );
-                console.log("날씨 이미지 : "+ resp.weather[0].icon );
-                console.log("바람   : "+ resp.wind.speed );
-                console.log("나라   : "+ resp.sys.country );
-                console.log("도시이름  : "+ resp.name );
-                console.log("구름  : "+ (resp.clouds.all) +"%" );                 
-            }
-        })
-    }
+$(document).ready(function () {
+    // 스타일을 변경합니다.
+    $('#box').css({
+        width: 100,
+        height: 10,
+        background: 'blue'
+    }).animate({
+        width: 900,
+        opacity: 0.5,
+    }, 2000);
+});
+
+
+
+
+// 변수를 선언합니다.
+var date = new Date();
+var hours = date.getHours();
+
+// 조건문
+if (hours < 12) {
+    // 표현식 "hours < 12"가 참일 경우 실행합니다.
+    document.write('오전입니다.');
+} else {
+    // 표현식 "hours < 12"가 거짓일 경우 실행합니다.
+    document.write('즐거운 오후입니다.');
+}
